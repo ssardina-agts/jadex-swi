625dfe626fc265306443a92247a3845d0e7a7376
Rene Fischer <s3401457@student.rmit.edu.au>
2013-11-27 08:22:33 +1100

    document PrologEngine interface

--- src/main/java/au/edu/rmit/csit/swijadex/PrologEngine.java
+++ src/main/java/au/edu/rmit/csit/swijadex/PrologEngine.java
@@ -20,16 +20,37 @@ public interface PrologEngine {
 	 */
 	public void consult(String filename);
 	
+	/*
+	 * create a query that can be executed later
+	 */
 	public Query query(String query);
 	
+	/*
+	 * use this to interpolate terms into a query
+	 * the query should have as many question marks ("?") as there are parameters after the query parameter
+	 * e.g.: query("father(?, ?)", "joseph", "jesus");
+	 * this is useful for user input, avoiding injection attacks
+	 */
 	public Query query(String query, Term... params);
 	
+	/*
+	 * add a fact before the other facts
+	 */
 	public void asserta(String fact);
 
+	/*
+	 * add a fact after the other facts
+	 */
 	public void assertz(String fact);
 
+	/*
+	 * remove a fact
+	 */
 	public void retract(String fact);
 
+	/*
+	 * remove all facts matching the parameter
+	 */
 	public void retractall(String fact);
 	
 	/*
@@ -37,28 +58,63 @@ public interface PrologEngine {
 	 */
 	public PrologEngine havingConsulted(String filename);
 	
+	/*
+	 * execute query and retrieve first solution
+	 * 
+	 * Only use this when you only want one solution. If you want more, use getAllSolutions or nSolutions
+	 */
 	public Hashtable<String, Term> getSolution(String query);
 
+	/*
+	 * use this to interpolate terms into a query
+	 * the query should have as many question marks ("?") as there are parameters after the query parameter
+	 * e.g.: getSolution("father(?, ?)", "joseph", "jesus");
+	 * this is useful for user input, avoiding injection attacks
+	 */
 	public Hashtable<String, Term> getSolution(String query, Term... params);
 	
-//	public Hashtable<String, Term> getSolution(String query, String... params);
-	
+	/*
+	 * use this if you only care about whether the query has a provable solution, but not what that solution actually is
+	 */
 	public boolean hasSolution(String query);
 	
+	/*
+	 * use this to interpolate terms into a query
+	 * the query should have as many question marks ("?") as there are parameters after the query parameter
+	 * e.g.: hasSolution("father(?, ?)", "joseph", "jesus");
+	 * this is useful for user input, avoiding injection attacks
+	 */
 	public boolean hasSolution(String query, Term... params);
 	
-//	public boolean hasSolution(String query, String... params);
-	
+	/*
+	 * use this to iterate through all solutions to a query
+	 */
 	public Hashtable<String, Atom>[] getAllSolutions(String query);
-	
+
+	/*
+	 * use this to interpolate terms into a query
+	 * the query should have as many question marks ("?") as there are parameters after the query parameter
+	 * e.g.: getAllSolutions("is_coloured(X, ?)", "yellow");
+	 * this is useful for user input, avoiding injection attacks
+	 */
 	public Hashtable<String, Atom>[] getAllSolutions(String query, Term... params);
 
+//  TODO : generalise to use Strings rather than Terms once we use multiple backends, since Terms are JPL specific
 //	public Hashtable<String, Atom>[] getAllSolutions(String query, String... params);
 
+	/*
+	 * this might be convenient for debugging
+	 */
 	public void dumpListing();
 	
+	/*
+	 * turn a java-side object into an ID that can be used in prolog to identify that object
+	 */
 	public Atom deflateObject(Object object);
 	
+	/*
+	 * turn a prolog-side ID into the java object it refers to
+	 */
 	public Object inflateAtom(Atom key);
 
 }
