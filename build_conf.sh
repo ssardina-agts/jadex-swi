cbfb748030accc274380852e91393ed1c545ce5a
Sebastian Sardina <ssardina@gmail.com>
2013-12-20 13:52:15 +1100

    Adjusted build_conf to standard values

--- build_conf.sh
+++ build_conf.sh
@@ -7,10 +7,10 @@
 
 
 # change this if you change the version of SWI-Jadex
-SWI_JADEX_VERSION=1.0
+SWI_JADEX_VERSION=6.2.6
 
 # point this to your jadex directory. It should contain a lib directory containing a number of jar files, like jadex-bridge-2.3.jar
-JADEX_DIR=~/jadex-2.3
+JADEX_DIR=/opt/jadex
 
 # point this towards your SWI-Prolog directory
 SWI_DIR=/usr/lib/swi-prolog
@@ -22,8 +22,8 @@ JPL_JAR=$SWI_DIR/lib/jpl.jar
 LIBJPL_DIR=$SWI_DIR/lib/amd64
 
 # point this towards your junit jar file
-JUNIT_JAR=~/opt/eclipse/plugins/org.junit_4.10.0.v4_10_0_v20120426-0900/junit.jar
+#JUNIT_JAR=~/opt/eclipse/plugins/org.junit_4.10.0.v4_10_0_v20120426-0900/junit.jar
+JUNIT_JAR=/usr/share/java/junit.jar
 
 # point this towards your hamcrest jar file
-HAMCREST_JAR=~/opt/eclipse/plugins/org.hamcrest.core_1.1.0.v20090501071000.jar
-
+HAMCREST_JAR=/usr/share/eclipse/dropins/jdt/plugins/org.hamcrest.core_1.3.0.jar
